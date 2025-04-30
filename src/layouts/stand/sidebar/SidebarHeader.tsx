7cb85b38482447a3969daf8d25786b7107f94d38
JunHyeok <dev.junh@gmail.com>
2025-04-30 23:43:03 +0900

    1. 로고 적용  - 똥쓰레기. 다시 찾자

--- src/layouts/stand/sidebar/SidebarHeader.tsx
+++ src/layouts/stand/sidebar/SidebarHeader.tsx
@@ -10,23 +10,33 @@ const SidebarHeader = forwardRef<HTMLDivElement, any>((props, ref) => {
   const lightLogo = () => (
     <Fragment>
       <Link to="/" className="dark:hidden">
+        <div className="default-logo flex items-center">
+          <img
+            src={toAbsoluteUrl('/media/brand-logos/marketing-standard-icon.svg')}
+            className="w-10 h-10 mr-3"
+            alt="마케팅의 정석"
+          />
+          <span className="text-gray-800 text-lg font-bold">마케팅의 정<span style={{ display: 'inline-block', borderBottom: '1.5px solid currentColor', width: '1em', position: 'relative', top: '-0.2em', marginLeft: '0.1em', marginRight: '0.1em' }}></span>석</span>
+        </div>
         <img
-          src={toAbsoluteUrl('/media/app/default-logo.svg')}
-          className="default-logo min-h-[22px] max-w-none"
-        />
-        <img
-          src={toAbsoluteUrl('/media/app/mini-logo.svg')}
-          className="small-logo min-h-[22px] max-w-none"
+          src={toAbsoluteUrl('/media/brand-logos/marketing-standard-icon.svg')}
+          className="small-logo w-10 h-10"
+          alt="마케팅의 정석"
         />
       </Link>
       <Link to="/" className="hidden dark:block">
+        <div className="default-logo flex items-center">
+          <img
+            src={toAbsoluteUrl('/media/brand-logos/marketing-standard-icon.svg')}
+            className="w-10 h-10 mr-3"
+            alt="마케팅의 정석"
+          />
+          <span className="text-white text-lg font-bold">마케팅의 정<span style={{ display: 'inline-block', borderBottom: '1.5px solid currentColor', width: '1em', position: 'relative', top: '-0.2em', marginLeft: '0.1em', marginRight: '0.1em' }}></span>석</span>
+        </div>
         <img
-          src={toAbsoluteUrl('/media/app/default-logo-dark.svg')}
-          className="default-logo min-h-[22px] max-w-none"
-        />
-        <img
-          src={toAbsoluteUrl('/media/app/mini-logo.svg')}
-          className="small-logo min-h-[22px] max-w-none"
+          src={toAbsoluteUrl('/media/brand-logos/marketing-standard-icon.svg')}
+          className="small-logo w-10 h-10"
+          alt="마케팅의 정석"
         />
       </Link>
     </Fragment>
@@ -34,13 +44,18 @@ const SidebarHeader = forwardRef<HTMLDivElement, any>((props, ref) => {
 
   const darkLogo = () => (
     <Link to="/">
+      <div className="default-logo flex items-center">
+        <img
+          src={toAbsoluteUrl('/media/brand-logos/marketing-standard-icon.svg')}
+          className="w-10 h-10 mr-3"
+          alt="마케팅의 정석"
+        />
+        <span className="text-white text-lg font-bold">마케팅의 정<span style={{ display: 'inline-block', borderBottom: '1.5px solid currentColor', width: '1em', position: 'relative', top: '-0.2em', marginLeft: '0.1em', marginRight: '0.1em' }}></span>석</span>
+      </div>
       <img
-        src={toAbsoluteUrl('/media/app/default-logo-dark.svg')}
-        className="default-logo min-h-[22px] max-w-none"
-      />
-      <img
-        src={toAbsoluteUrl('/media/app/mini-logo.svg')}
-        className="small-logo min-h-[22px] max-w-none"
+        src={toAbsoluteUrl('/media/brand-logos/marketing-standard-icon.svg')}
+        className="small-logo w-10 h-10"
+        alt="마케팅의 정석"
       />
     </Link>
   );
@@ -48,7 +63,7 @@ const SidebarHeader = forwardRef<HTMLDivElement, any>((props, ref) => {
   return (
     <div
       ref={ref}
-      className="sidebar-header hidden lg:flex items-center relative justify-between px-3 lg:px-6 shrink-0"
+      className="sidebar-header hidden lg:flex items-center relative justify-between px-3 lg:px-6 shrink-0 py-3"
     >
       {layout.options.sidebar.theme === 'light' ? lightLogo() : darkLogo()}
       <SidebarToggle />

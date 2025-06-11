a3f307725c03294deb96cce07205f393b121d05d
jhjang <dev.junh@gmail.com>
2025-06-11 16:50:38 +0900

    [수정사항 반영] 1. 비기너(beginner) 권한 관련  - 색상 변경  - AuthProvider authStore 관련 supabase 406 에러 수정 : 비기너 권한은 auth.users 에만 존재하고 있어 오류가 발생하고 있었음, 우선 예외처리 2. 캐시 출금 신청  - 메뉴명 변경, StyleToolbar 내용 변경  - 버튼 디자인 일원화 : 전액 버튼 bg-card 클래스 누락, 크기 문제 해결 3. 내정보 관리 페이지  - 이미지 모달 수정  - 등업 신청 모달 구성 변경   > DialogFooter 반영 :DialogContent 영역에서만 스크롤링   > 필수값 데이터 사전 Validation 처리 추가 4. 기타

--- src/config/menu.config.tsx
+++ src/config/menu.config.tsx
@@ -368,7 +368,7 @@ export const MENU_SIDEBAR: TMenuConfig = [
     ]
   },
   {
-    title: '총판 출금 신청',
+    title: '캐시 출금 신청',
     icon: 'dollar text-danger',
     path: '/withdraw',
     authCheck: (role) => role === USER_ROLES.DISTRIBUTOR || role === USER_ROLES.DEVELOPER,  // 총판, 개발자
